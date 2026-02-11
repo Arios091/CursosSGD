ec4dbc7854a8e949f2e80f7c146dc2111b0aa774
Arios091 <ayrton.rios@unas.edu.pe>
2026-02-11 11:01:59 -0500

    modificacion del .env.example

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,15 @@
+@extends('main.app')
+
+@section('content')
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi Proyecto UNAS</title>
+    </head>
+<body>
+    <nav> </nav>
+
+    <div class="container">
+        @yield('content')
+@endsection
